dc2cee8a2498149445131c8b7fe97f0de2b1150c
Brian Kaemingk <brian.e.k@gmail.com>
2015-12-06 21:51:29 -0800

    Remove leading or trailing whitespace from band names

--- app/models/band_name.rb
+++ app/models/band_name.rb
@@ -1,5 +1,7 @@
 class BandName < ActiveRecord::Base
 
+  before_save{self.title=title.strip}
+
   belongs_to :user
   validates :title, presence: true, length: {minimum:1}
   validates :user_id, presence: true
